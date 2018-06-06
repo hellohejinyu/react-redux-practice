cbc0dde50ac6bb9d84bac70e9a1516c565364d0c
kaijun <cadenho@hotmail.com>
2018-06-06 18:22:25 +0800

    划分containers和components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import Header from './Header'
-import Content from './Content'
+import Header from './containers/Header'
+import Content from './containers/Content'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <Header name='头部' />
-        <Content name='内容' />
+        <Header />
+        <Content />
       </div>
     )
   }
--- src/containers/Content.js
+++ src/containers/Content.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import ThemeSwitch from './ThemeSwitch'
 import { connect } from 'react-redux'
 
 class Content extends Component {
-  render() {
+  static propTypes = {
+    themeColor: PropTypes.string
+  }
+
+  render () {
     return (
       <div>
         <p style={{ color: this.props.themeColor }}>React.js 小书内容</p>
-        <ThemeSwitch name='switch' />
+        <ThemeSwitch />
       </div>
     )
   }
 }
 
-const WrapContent = connect(state => ({
-  themeColor: state.themeColor
-}))(Content)
-
-export default WrapContent
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+export default connect(mapStateToProps)(Content)
